3cd614ffd77ea508080209adda13e0e88d0084e1
jvink10 <jordanvink10@gmail.com>
2023-09-18 23:50:50 +1000

    Add: navbar

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Version 1.0.0
 # To Add:
 
 ## Features
-- Home page visual
-- Header and footer
+- Add home page content
+- Add header and footer content
+- Add create page
+- Add room page
+- Add photosphere page
 - Add loading indication
 - Visual theme - grid planning paper
-- Add date/time to photospheres
-- Background adding
-- Photo sphere adding
+- Add date/time to photospheres or other criteria such as weather
 - Add background layering
 - Modifiable by non-programmers
 - Add accounts and storage
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+import Link from 'next/link';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,8 +19,12 @@ export default function RootLayout({
   	return (
     	<html lang="en">
       		<body className={`${inter.className} flex flex-col min-h-screen`}>
-				<header className="border-b border-gray-100">
-					<h1 className=" py-2 px-8 text-lg">Room View</h1>
+				<header className="flex flex-row justify-evenly border-b border-gray-100">
+					<h1 className="py-2 text-lg">Room View</h1>
+					<div className="my-auto space-x-8">
+						<Link href="/">Home</Link>
+						<Link href="/create">Create</Link>
+					</div>
 				</header>
 				<div className="flex-grow">
 					{children}
