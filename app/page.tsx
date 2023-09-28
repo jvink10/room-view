9ed264f973217979c8531d12fdf7c1c5b2cdd9ff
jvink10 <jordanvink10@gmail.com>
2023-09-28 15:16:24 +1000

    Fix: photoSphere -> photosphere

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,20 @@ export default function Home() {
     	<main>
 			<section className="my-8 mx-auto border-2 border-gray-300 w-fit">
 				<Room
+					id={2}
+					name="Example Room"
 					image="/background.png"
 					height={849}
 					width={985}
-					photoSphere={[
-						{image: "/bardon-esplanade-park.jpg", topPos: "75%", leftPos: "20%", color: "bg-green-500"},
-						{image: "/bardon-park-bridge.jpg", topPos: "68%", leftPos: "68%", color: "bg-blue-400"},
-						{image: "/bowman-park.jpg", topPos: "93%", leftPos: "53%", color: "bg-green-500"},
-						{image: "/dawn-street-park.jpg", topPos: "30%", leftPos: "58%"},
-						{image: "/glen-harding-park.jpg", topPos: "20%", leftPos: "58%"},
-						{image: "/lions-park.jpg", topPos: "10%", leftPos: "80%"},
-						{image: "/lions-park-parking.jpg", topPos: "22%", leftPos: "67%"},
-						{image: "/st-josephs-lunch-area.jpg", topPos: "86%", leftPos: "66%", color: "bg-yellow-400"},
+					photosphere={[
+						{id: 1, name: "Bardon Esplanade Park", image: "/bardon-esplanade-park.jpg", topPos: "75%", leftPos: "20%", color: "bg-green-500"},
+        				{id: 2, name: "Bardon Park Bridge", image: "/bardon-park-bridge.jpg", topPos: "68%", leftPos: "68%", color: "bg-blue-400"},
+        				{id: 3, name: "Bowman Park", image: "/bowman-park.jpg", topPos: "93%", leftPos: "53%", color: "bg-green-500"},
+        				{id: 4, name: "Dawn Street Park", image: "/dawn-street-park.jpg", topPos: "30%", leftPos: "58%"},
+        				{id: 5, name: "Glen Harding Park", image: "/glen-harding-park.jpg", topPos: "20%", leftPos: "58%"},
+        				{id: 6, name: "Lions Park", image: "/lions-park.jpg", topPos: "10%", leftPos: "80%"},
+        				{id: 7, name: "Lions Park Parking", image: "/lions-park-parking.jpg", topPos: "22%", leftPos: "67%"},
+        				{id: 8, name: "St. Josephs Lunch Area", image: "/st-josephs-lunch-area.jpg", topPos: "86%", leftPos: "66%", color: "bg-yellow-400"},
 					]}
 				/>
 			</section>
