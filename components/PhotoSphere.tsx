fbe6002f82d2ab13596bed39e3470798afecf0ea
jvink10 <jordanvink10@gmail.com>
2023-09-29 20:51:49 +1000

    Update: Photosphere dimensions reliant on parent

--- components/PhotoSphere.tsx
+++ components/PhotoSphere.tsx
@@ -15,11 +15,11 @@ export default function PhotoSphere(props: Props) {
     return (
         <div className={`absolute group border-2 rounded-full border-white h-4 w-4 ${props.color ? props.color : "bg-gray-400"}`} style={{top: `${props.topPos}`, left: `${props.leftPos}`}}>
             <div className={`relative rounded-full h-4 w-4 -top-0.5 -left-0.5 bg-white ${props.ping ? "animate-ping" : "hidden"}`}></div>
-            <div className="relative hidden group-hover:block -top-[148px] -left-[148px] z-10">
+            <div className="relative hidden group-hover:block h-[300px] w-[300px] -top-[148px] -left-[148px] z-10">
                 <ReactPhotoSphereViewer
                     src={props.image}
-                    width="300"
-                    height="300"
+                    height={"100%"}
+                    width={"100%"}
                     container={''}
                     fisheye={true}
                     navbar={['fullscreen']}
