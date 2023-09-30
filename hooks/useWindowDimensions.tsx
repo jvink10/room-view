a87007dc8c4062568aaa01d522744683ddfdda48
jvink10 <jordanvink10@gmail.com>
2023-09-30 19:02:25 +1000

    Add: useWindowDimensions hook

---
+++ hooks/useWindowDimensions.tsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export default function useWindowDimensions() {
+    const hasWindow = typeof window !== 'undefined';
+
+    function getWindowDimensions() {
+        const height = hasWindow ? window.innerHeight : null;
+        const width = hasWindow ? window.innerWidth : null;
+        return {height, width};
+    };
+
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+    function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+    };
+
+    useEffect(() => {
+        if (hasWindow) {
+            handleResize();
+
+            window.addEventListener('resize', handleResize);
+            return () => window.removeEventListener('resize', handleResize);
+        };
+    }, [hasWindow]);
+
+    return windowDimensions;
+};
